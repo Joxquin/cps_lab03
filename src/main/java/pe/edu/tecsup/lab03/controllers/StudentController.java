d6a49117a39e42fe3d025f4577082d7f6bcebd9b
Joxquin <jotajoxquin@gmail.com>
2026-09-04 19:44:24 -0500

    4to commit

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,6 +2,6 @@ package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
     StudentController() {
-        System.out.println("Terminamos el laboratorio!");
+        System.out.println("Terminamos el laboratorio yaaaa!");
     }
 }
